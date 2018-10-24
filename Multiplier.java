0d0aba11d54d8cce9c09e051434cca292a99c707
Arvind V <arvindvishak@gmail.com>
2018-10-24 14:26:48 -0400

    Add files via upload
    problem 74 : multiplier class

---
+++ Multiplier.java
@@ -0,0 +1,24 @@
+public class Multiplier {
+
+    public static void main(String[] args) {
+        Multiplier threeMultiplier = new Multiplier(3);
+        System.out.println("threeMultipler.multiply(2) : " + threeMultiplier.multiply(2));
+
+        Multiplier fourMultiplier = new Multiplier(4);
+        System.out.println("threeMultiplier.multiply(2): " + fourMultiplier.multiply(2));
+
+        System.out.println("threeMultiplier.multiply(1): " + threeMultiplier.multiply(1));
+        System.out.println("fourMultiplier.multiply(1): " + fourMultiplier.multiply(1));
+    }
+
+    private int number;
+
+    public Multiplier(int initialNumber) {
+
+        this.number = initialNumber;
+    }
+
+    public int multiply(int otherNumber) {
+        return otherNumber * this.number;
+    }
+}
